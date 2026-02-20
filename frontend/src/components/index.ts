6315e3a048a040b0765541b2768ba6a7d117e1b3
ONEONUORA <monweebube44@gmail.com>
2026-02-20 21:59:11 +0100

    feat: add about us page

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -6,3 +6,6 @@ export { Navbar } from './layout/Navbar';
 export { Footer } from './layout/Footer';
 export { FAQ } from './contact/FAQ';
 export { ContactSection } from './contact/ContactSection';
+export { HowItWorks } from './marketing/HowItWorks';
+export { TeamCard } from './marketing/TeamCard';
+export { Roadmap } from './marketing/Roadmap';
